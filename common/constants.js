09ae60213a0c0c5fdb48beb6629f3a388a8e180c
satyajit nayak <satyajitn15@gmail.com>
2024-03-09 11:56:19 +0530

    add samples.json to gitignore

--- common/constants.js
+++ common/constants.js
@@ -6,8 +6,11 @@ constants.DATASET_DIR = `${constants.DATA_DIR}/dataset`
 constants.JSON_DIR = `${constants.DATASET_DIR}/json`
 constants.IMG_DIR = `${constants.DATASET_DIR}/img`
 constants.SAMPLES = `${constants.DATASET_DIR}/samples.json`
+constants.FEATURES = `${constants.DATASET_DIR}/features.json`
 constants.JS_OBJECTS = '../common/js_objects'
 constants.SAMPLE_JS = `${constants.JS_OBJECTS}/samples.js`
+constants.FEATURES_JS = `${constants.JS_OBJECTS}/features.js`
+
 
 if (typeof module !== "undefined") {
   module.exports = constants
---
+++ common/features.js
@@ -0,0 +1,15 @@
+const features = {}
+
+features.getPathCount = (paths) => {
+  return paths.length
+}
+
+
+features.getPointCount = (paths) => {
+  const points = paths.flat()
+  return points.length
+}
+
+if (typeof module !== "undefined") {
+  module.exports = features
+}
